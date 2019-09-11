550ea2cb94e93d056165a9fc327fedfcfcc27bde
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-11 16:18:03 +0300

    DriverLicenseIssueCountryCodeInterface.

---
+++ src/Contracts/References/DriverLicenseIssueCountryCodeInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Likemusic\YandexFleetTaxiClient\Contracts\References;
+
+interface DriverLicenseIssueCountryCodeInterface
+{
+    const RUSSIA = 'Россия';
+    const BELARUS = 'Белоруссия';
+    const KAZAKHSTAN = 'Казахстан';
+    const KYRGYZSTAN = 'Киргизия';
+}
