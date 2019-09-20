93342e99c185e6002b9440e8bb959d826cae1870
Valerij Ivashchenko <likemusicdev@gmail.com>
2019-09-21 02:47:25 +0300

    Add DriverProfile/WorkStatusInterface.

---
+++ src/Contracts/PostDataKey/CreateDriver/DriverProfile/WorkStatusInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Likemusic\YandexFleetTaxiClient\Contracts\PostDataKey\CreateDriver\DriverProfile;
+
+
+interface WorkStatusInterface
+{
+    const WORKING = 'working';
+    const NOT_WORKING = 'not_working';
+    const FIRED = 'fired';
+}
